a047fe6d43e5ca0d9b61caf2fc128aa304ebc10d
unknown <Lucas.campau@gmail.com>
2025-02-07 16:43:01 -0500

    fixed save conflict in elevatorConstants

--- 2025BBot/src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
+++ 2025BBot/src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -1,22 +1,40 @@
 package frc.robot.subsystems.Elevator;
 
 public final class ElevatorConstants {
+    
 
-  // default home position
-  public static final double homePos = 0; // placeholder
+    //motor ids
+    public static final int leftMotorID = 15;
+    public static final int rightMotorID = 14;
 
-  // each coral branch height
-  public static final double l1Pos = 0; // placeholder
-  public static final double l2Pos = 0; // placeholder
-  public static final double l3Pos = 0; // placeholder
-  public static final double l4Pos = 0; // placeholder
+    //limit switch ids
+    public static final int limitSwitch1ID = 0;
+    public static final int limitSwitch2ID = 1; 
 
-  // algae on reef heights
-  public static final double algaeL2Pos = 0; // placeholder
-  public static final double algaeL3Pos = 0; // placeholder
+    //PIDs
+    public static final double leftMotorP = 0; //placeholder
+    public static final double rightMotorP = 0; //placeholder
+    public static final double leftMotorI = 0; //placeholder
+    public static final double rightMotorI = 0; //placeholder
+    public static final double leftMotorD = 0; //placeholder
+    public static final double rightMotorD = 0; //placeholder
 
-  // miscellanious positions
-  public static final double sourcePos = 0; // placeholder
-  public static final double bargePos = 0; // placeholder
-  public static final double processorPos = 0; // placeholder
+    //default home position
+    public static final double homePos = 0;  //placeholder
+
+    //each coral branch height
+    public static final double l1Pos = 0; //placeholder
+    public static final double l2Pos = 0; //placeholder
+    public static final double l3Pos = 0; //placeholder
+    public static final double l4Pos = 0; //placeholder
+
+    //algae on reef heights
+    public static final double algaeL2Pos = 0; //placeholder
+    public static final double algaeL3Pos = 0; //placeholder
+
+    //misc positions
+    public static final double sourcePos = 0; //placeholder
+    public static final double bargePos = 0; //placeholder
+    public static final double processorPos = 0; //placeholder 
+
 }
