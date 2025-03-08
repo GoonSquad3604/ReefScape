f217ec8f25b3cc41d06ccc95f7fe551cbc5072ee
Simon <phillips.se2020@gmail.com>
2025-03-08 16:10:32 -0500

    simon forgot to push code for at least the 5th time -andrew

--- LAbotJames/src/main/java/frc/robot/subsystems/drive/ModuleIO.java
+++ LAbotJames/src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -29,6 +29,7 @@ public interface ModuleIO {
     public boolean turnEncoderConnected = false;
     public Rotation2d turnAbsolutePosition = new Rotation2d();
     public Rotation2d turnPosition = new Rotation2d();
+    public double turnPositionRaw = 0.0;
     public double turnVelocityRadPerSec = 0.0;
     public double turnAppliedVolts = 0.0;
     public double turnCurrentAmps = 0.0;
